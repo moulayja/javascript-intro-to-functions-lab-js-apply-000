47e43cb45de9141a917ae6f049e12f699d86ea16
Jamal Farah <moulayjam@gmail.com>
2019-05-25 04:00:42 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -35,9 +35,9 @@ describe('logWhisper(string)', function() {
   })
 })
 
-function sayHiToGrandma(string){
-  if(string ==='hello') {`I can\'t hear you!" if ${string} is lowercase`}
-  "I can't hear you!")
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
 
   it('returns "YES INDEED!" if `string` is uppercase', function() {
